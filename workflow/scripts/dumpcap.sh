db53534f17a661948317038c6bd70b92079879ac
Joe Hopkinson <joe@hopkinson.co.uk>
2021-06-01 14:46:13 +0200

    Added timestamp

--- workflow/scripts/dumpcap.sh
+++ workflow/scripts/dumpcap.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-dumpcap -w /home/joe/catkin_ws/src/data/dumpcap/data -f "host turtlebot"
+NOW=$(date +"%m-%d-%Y")
 
+dumpcap -w /home/joe/catkin_ws/src/data/dumpcap/data.$NOW -f "host turtlebot"
